e49bd27fd57035e72da835d0bb1f9a8ea83532c5
kevin hernon <kevinhernon@kevins-MacBook-Air.local>
2016-06-21 19:31:44 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,72 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+   
+  def initialize(options={})
+    options.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
+      SQL
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    #format into sql needs 
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name) == nil
+    end
+    values.join(", ")
+  end
+
+  def col_names_for_insert
+   self.class.column_names.reject { |col_name| col_name == 'id' }.join(", ")
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name} WHERE name = '#{name}'
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.find_by(options={})
+    #need to iterate through the entire db until we find the value passed 
+    #split the options into key and value pair
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name} WHERE #{options.keys.join("")} = '#{options.values.join("")}'
+      SQL
+       #binding.pry
+      vals = DB[:conn].execute(sql)     
+  end
+
 end
